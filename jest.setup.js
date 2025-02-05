4a750c3a413284e6195eb5f1575ee6a4a426f247
emerick-biron <emerick.biron@gmail.com>
2025-02-05 19:30:45 +0100

    add tests

--- jest.setup.js
+++ jest.setup.js
@@ -3,3 +3,11 @@ import "@testing-library/jest-dom";
 
 global.TextEncoder = TextEncoder;
 global.TextDecoder = TextDecoder;
+Element.prototype.scrollTo = jest.fn();
+global.BroadcastChannel = class {
+    constructor() { }
+    postMessage() { }
+    close() { }
+    addEventListener() { }
+    removeEventListener() { }
+};
